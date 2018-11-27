10c57976ca8806cc6b8c56fefcc71b150eff3df2
Carlos Muñoz <cmunozgar@gmail.com>
2018-11-27 19:41:18 +0100

    Delete Rows

--- lib/coda_docs/client/rows.rb
+++ lib/coda_docs/client/rows.rb
@@ -9,6 +9,10 @@ module CodaDocs
       def row(doc_id, table_id, row_id, options = {})
         self.class.get("/docs/#{doc_id}/tables/#{table_id}/rows/#{row_id}", query: options)
       end
+
+      def delete_row(doc_id, table_id, row_id)
+        self.class.delete("/docs/#{doc_id}/tables/#{table_id}/rows/#{row_id}")
+      end
     end
   end
 end
