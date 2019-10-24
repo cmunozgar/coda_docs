728d6e07a537b34bda27286e4d359749b1a8f02c
cmunozgar <cmunozgar@gmail.com>
2019-10-24 15:27:30 +0200

    Deleted Useless spec

--- spec/coda_docs_spec.rb
+++ spec/coda_docs_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe CodaDocs do
   it 'has a version number' do
     expect(CodaDocs::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(false)
-  end
 end
