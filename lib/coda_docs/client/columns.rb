079b718ec69552b0ef0d2d6fce0b0dd77f5c10f7
cmunozgar <cmunozgar@gmail.com>
2019-10-31 16:01:07 +0100

    Added support for columns and rows of a View

--- lib/coda_docs/client/columns.rb
+++ lib/coda_docs/client/columns.rb
@@ -9,6 +9,11 @@ module CodaDocs
       def column(doc_id, table_id, column_id, options = {})
         self.class.get("/docs/#{doc_id}/tables/#{table_id}/columns/#{column_id}", query: options)
       end
+
+      def view_columns(doc_id, view_id, options = {})
+        response = self.class.get("/docs/#{doc_id}/views/#{view_id}/columns", query: options)
+        response.parsed_response['items']
+      end
     end
   end
 end
