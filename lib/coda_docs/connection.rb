9e82635c56264771937a34bc0d574909600142a1
Carlos Muñoz <cmunozgar@gmail.com>
2020-08-17 10:33:33 +0200

    Support new version 1 of Coda API

--- lib/coda_docs/connection.rb
+++ lib/coda_docs/connection.rb
@@ -1,7 +1,7 @@
 module CodaDocs
   class Connection
     include HTTParty
-    base_uri 'https://coda.io/apis/v1beta1'
+    base_uri 'https://coda.io/apis/v1'
     format :json
   end
 end
---
+++ lib/coda_docs/entities/pages.rb
@@ -0,0 +1,26 @@
+module CodaDocs
+  module Entities
+    class Pages < Entity
+      def all(doc_id)
+        response = connection.get("/docs/#{doc_id}/pages", query: options)
+        parse_response(response)
+      end
+
+      def find(doc_id:, page_id:)
+        connection.get("/docs/#{doc_id}/pages/#{page_id}", query: options)
+      end
+
+      def update(doc_id:, page_id:, name:, subtitle:, icon_name:, image_url:)
+        connection.put(
+          "/docs/#{doc_id}/pages/#{page_id}",
+          body: {
+            name: name,
+            subtitle: subtitle,
+            iconName: icon_name,
+            imageUrl: image_url
+          }.to_json
+        )
+      end
+    end
+  end
+end
